d511e858be4c650334a4e335e20e1fdc3f0ee02e
Srestituyo <sebastianrestituyo@gmail.com>
2022-10-14 10:45:12 -0400

    AddingRegexSSNValidator

---
+++ SSNValidator/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information 
+ // Validate SSN
+
+using System.Text.RegularExpressions;
+
+Regex validateSSNRegex = new Regex("^(?!666|000|9\\d{2})\\d{3}-(?!00)\\d{2}-(?!0{4})\\d{4}$");
+Console.Write("Ingrese el SSN: ");
+var aSSN = Console.ReadLine();
+Console.WriteLine(validateSSNRegex.IsMatch(aSSN));  // prints True
+         
